241b26f67465b82a18b6049e5fc1e3cb95504803
TurinAleksey <turinalpi28@yandex.ru>
2025-06-12 15:57:51 +0300

    Test commit

---
+++ Lr1/Turin_A_A_24BMz/Kod.py
@@ -0,0 +1,8 @@
+def to_uppercase():
+    s = input("Введите строку: ")  
+    result = s.upper()  
+    print("Результат:", result)  
+to_uppercase()
+input("Нажмите enter, чтобы выйти")
+
+# Тюрин Алексей Александрович ВМЗ-24
